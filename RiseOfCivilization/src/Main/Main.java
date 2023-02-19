e0eedb1d237194097b1b8acd2871ef95baf2f044
Jvaljer <abbel.haol@gmail.com>
2023-02-19 17:56:46 +0100

    error in the Shortest Path algorithm... -> must repair

--- RiseOfCivilization/src/Main/Main.java
+++ RiseOfCivilization/src/Main/Main.java
@@ -3,6 +3,8 @@ package Main;
 import Model.*;
 import View.*;
 import Controler.*;
+import java.awt.*;
+import java.util.*;
 
 public class Main {
 
@@ -10,7 +12,13 @@ public class Main {
 		GameModel model = new GameModel();
 		GameView view = new GameView(model);
 		GameCtrl ctrl = new GameCtrl(view);
-
+		
+		Point start = new Point(0,0);
+		Point end = new Point(4,2);
+		ArrayList<Point> path = model.GetMapModel().GetShortestPath(start, end);
+		for(int i=0; i<path.size(); i++) {
+			System.out.println("path[" + i + "] = (" + path.get(i).x + "," + path.get(i).y + ")");
+		}
 	}
 
 }
