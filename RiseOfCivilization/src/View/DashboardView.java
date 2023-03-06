01e0d0cea4188bd2ce17cc989c2dac48d8ed73a2
Amaury Ruelle <amaury.ruelle@universite-paris-saclay.fr>
2023-03-06 16:39:54 +0100

    Ajout des boutons dans le dashboard_view

---
+++ RiseOfCivilization/src/View/DashboardView.java
@@ -0,0 +1,40 @@
+package View;
+
+import Model.MapModel;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+
+/**
+ * This class is for the view of the dashboard. The dashboard is represented in
+ * a seperate panel at the right of the map and contains from top to bottom :
+ * the minimap, the inventory and information about the selected cell/worker.
+ * Its default height is the height of the map and its default width is a
+ * third of the width of the map.
+ *
+ * @author martin
+ */
+@SuppressWarnings("serial")
+public class DashboardView extends JPanel {
+	private MapModel map_model;
+	private MinimapView minimap_view;
+	private InventoryView inventory_view;
+	private InfobarView infobar_view;
+	
+	public DashboardView(MapModel mm, MinimapView mmv, InventoryView iv, InfobarView ibv) {
+		map_model = mm;
+		minimap_view = mmv;
+		inventory_view = iv;
+		infobar_view = ibv;
+		
+		setPreferredSize(new Dimension(map_model.GetWidth()/3, map_model.GetHeight()));
+		setLayout(new BorderLayout());
+		
+		add(minimap_view, BorderLayout.NORTH);
+		add(inventory_view, BorderLayout.CENTER);
+		add(infobar_view, BorderLayout.SOUTH);
+		
+	}
+}
