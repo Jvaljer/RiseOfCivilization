7b875e766652c7c212aed74b85429b5ff1bb9754
Jvaljer <abbel.haol@gmail.com>
2023-03-28 00:13:04 +0200

    starting to implement New Workers functions

--- RiseOfCivilization/src/Controler/ActionTrain.java
+++ RiseOfCivilization/src/Controler/ActionTrain.java
@@ -3,19 +3,26 @@ package Controler;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Model.MapModel;
+import Model.CellModel;
+import Model.BuildingModel;
 
 public class ActionTrain implements ActionListener {
 	private MapModel map;
+	private GameCtrl g_ctrl;
 	
 	public ActionTrain(GameCtrl ctrl) {
-		map = ctrl.GetGameModel().GetMapModel();
+		g_ctrl = ctrl;
+		map = g_ctrl.GetGameModel().GetMapModel();
 	}
 
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 			System.out.println("clicked New_Worker button");
-			//must implement
+			CellModel cell = map.GetCurrentCell();
+			BuildingModel building = map.GetBuildingFromCoord(cell.GetCoord());
+			
+			(new BuildingTrain(g_ctrl,building)).start();
 	}
 
 }
