7729ae77835a6eca0e6603c1a4ace1b3535a577e
Jvaljer <abbel.haol@gmail.com>
2023-04-02 23:34:45 +0200

    started to implement Shop Interface

--- RiseOfCivilization/src/View/ShopView.java
+++ RiseOfCivilization/src/View/ShopView.java
@@ -1,8 +1,33 @@
 package View;
 
 import javax.swing.JFrame;
+import java.awt.*;
+import Model.ShopModel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class ShopView extends JFrame{
+	private GameView game;
+	private ShopModel model;
+	
+	public ShopView(GameView GV, ShopModel SM) {
+		game = GV;
+		model = SM;
+		
+		setTitle("Shop Interface");
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout());
+		
+		JPanel pane = new JPanel();
+		//set the wanted buttons (sell & buy)
+		JButton button = new JButton();
+		//set the wanted menus (resource type) 
+		
+		//set the price indicator
+		
+		add(pane);
+	}
+	
 	
 	@Override
 	public void paint(Graphics G) {
